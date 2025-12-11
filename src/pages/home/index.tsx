245588e2017b9806c470df411026a8c8b8c8f97b
lucasterencio <lucasnn.145@outlook.com>
2025-12-11 08:59:50 -0300

    update name

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -84,7 +84,7 @@ export const Home = () => {
   return (
     <div className="flex flex-col w-full py-4 items-center justify-center">
       <h1 className="md:text-4xl text-3xl font-bold text-white mt-20">
-        Sujeito Programador
+        Lucas Terêncio
       </h1>
       <span className="text-gray-50 mb-5 mt-3">Veja meus Links 👇</span>
 
